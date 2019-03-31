4067c5de0e6b3cd47edcfdb50d74d75ffcaa34db
Tony Singer <singertony7@gmail.com>
2019-03-31 06:02:54 -0400

    Find nearest building button

--- app/src/main/java/com/hfad/seahawktours/Building.java
+++ app/src/main/java/com/hfad/seahawktours/Building.java
@@ -9,22 +9,26 @@ public class Building {
     private int imageResourceId;
     private int imageResourceId2;
     private int url;
+    private Double latitude;
+    private Double longitude;
 
     public static final Building[] buildings = {
-            new Building( R.string.alderman_hall, R.string.Alderman, R.string.ald_name, R.drawable.aldermanhall, R.drawable.eaalderman, R.string.ald_url ),
-            new Building(R.string.belk_hall, R.string.Belk, R.string.belk_name, R.drawable.belkhall1, R.drawable.irwinbelk, R.string.belk_url),
-            new Building(R.string.bear_hall, R.string.Bear , R.string.bear_name, R.drawable.bear, R.drawable.bear2, R.string.bear_url),
-            new Building(R.string.cis, R.string.Cis, R.string.cis_name, R.drawable.cis1, R.drawable.cis2, R.string.cis_url ),
-            new Building(R.string.bluthenthal, R.string.Bluthenthal , R.string.bluthenthal_name, R.drawable.bluth, R.drawable.bluth2, R.string.bluthenthal_url)
+            new Building(R.string.alderman_hall, R.string.Alderman, R.string.ald_name, R.drawable.aldermanhall, R.drawable.eaalderman, R.string.ald_url, 34.226838, -77.876943 ),
+            new Building(R.string.belk_hall, R.string.Belk, R.string.belk_name, R.drawable.belkhall1, R.drawable.irwinbelk, R.string.belk_url, 34.221944, -77.871404 ),
+            new Building(R.string.bear_hall, R.string.Bear , R.string.bear_name, R.drawable.bear, R.drawable.bear2, R.string.bear_url, 34.228627, -77.872819),
+            new Building(R.string.cis, R.string.Cis, R.string.cis_name, R.drawable.cis1, R.drawable.cis2, R.string.cis_url, 34.226131, -77.871788 ),
+            new Building(R.string.bluthenthal, R.string.Bluthenthal , R.string.bluthenthal_name, R.drawable.bluth, R.drawable.bluth2, R.string.bluthenthal_url, 34.224309, -77.871365)
     };
 
-    private Building(int name, int description, int addinfo, int imageResourceId, int imageResourceId2, int url){
+    private Building(int name, int description, int addinfo, int imageResourceId, int imageResourceId2, int url, Double latitude, Double longitude){
         this.name=name;
         this.description=description;
         this.addinfo=addinfo;
         this.imageResourceId=imageResourceId;
         this.imageResourceId2=imageResourceId2;
         this.url=url;
+        this.latitude=latitude;
+        this.longitude=longitude;
     }
 
     public int getName() {
@@ -50,4 +54,31 @@ public class Building {
     public int getUrl() {
         return url;
     }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public int getClosestBuilding(Double latitude, Double longitude){
+
+        Building closeBuilding = null;
+        Double shortestDistance = 100.00;
+
+        for (Building b : buildings){
+            if(distance(latitude, longitude, b.latitude, b.longitude) < shortestDistance){
+                shortestDistance = distance(latitude, longitude, b.latitude, b.longitude);
+                closeBuilding = b;
+            }
+        }
+        return closeBuilding.name;
+    }
+
+    public Double distance(Double x1, Double y1, Double x2, Double y2){
+        return Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+    }
+
 }
--- app/src/main/java/com/hfad/seahawktours/MainActivity.java
+++ app/src/main/java/com/hfad/seahawktours/MainActivity.java
@@ -1,24 +1,61 @@
 package com.hfad.seahawktours;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.content.Intent;
 import android.widget.TextView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.widget.ListView;
+import android.widget.AdapterView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static com.hfad.seahawktours.Building.buildings;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private String last_building = null;
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//        ArrayAdapter<Building> buildingArrayAdapter = new ArrayAdapter<>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                Building.buildings);
+
+        //create OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                        String selection = listView.getSelectedItem().toString();
+                        intent.putExtra( "retrieve", selection);
+                        startActivity(intent);
+                    }
+                };
+        ListView listView = findViewById(R.id.building);
+        //listView.setAdapter(buildingArrayAdapter);
+        listView.setOnItemClickListener(itemClickListener);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //if activity restarts last building selected is still visible
@@ -41,19 +78,6 @@ public class MainActivity extends AppCompatActivity {
         String saveLast =  previous_selection.getText().toString();
     }
 
-    public void onClickBuilding(View view) {
-        //grabs value from spinner
-        Spinner building = findViewById(R.id.building);
-        String selectedBuilding = building.getSelectedItem().toString();
-        last_building = selectedBuilding;
-        //passes selected value to "last building" text view
-        TextView previous_selection = findViewById(R.id.last_building);
-        previous_selection.setText(last_building);
-        //passes selected value to detail activity
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra( "retrieve", selectedBuilding);
-        startActivity(intent);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -70,5 +94,52 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    public Double distance(Double x1, Double y1, Double x2, Double y2){
+        return Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+    }
+
+    public void onNearestClick(View view){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            onNearestClick(view);
+        }
+        else{
+            //get location and pass intent
+
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+
+                                Double latitude = location.getLatitude();
+                                Double longitude = location.getLongitude();
+
+                                Building closeBuilding = null;
+                                Double shortestDistance = 100.00;
+
+                                for (Building b : buildings){
+                                    if(distance(latitude, longitude, b.getLatitude(), b.getLongitude()) < shortestDistance){
+                                        shortestDistance = distance(latitude, longitude, b.getLatitude(), b.getLongitude());
+                                        closeBuilding = b;
+                                    }
+                                }
+                                //pass building to DetailActivity
+                                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                                String closeBuildingName = getString(closeBuilding.getName());
+                                intent.putExtra( "retrieve", closeBuildingName);
+                                startActivity(intent);
+                            }
+                            //if null do what?
+                        }
+                    });
+
+        }
+    }
+
 }
 
